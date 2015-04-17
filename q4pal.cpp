78f03be930a8deff9f296457a4acd87ab975a56e
carolinarmtz <A01229797@itesm.mx>
2015-04-16 21:00:59 -0500

    Create q4pal.cpp

---
+++ q4pal.cpp
@@ -0,0 +1,26 @@
+// CarolinaRomo A01229797 
+// Palindrome Correction
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void palindrome (){
+	string x;
+	string y;
+	cout<<" enter string:";
+	cin >> x;
+	y= string(x.rbegin(),x.rend());
+	cout << "reverse string is:"<< y << endl;
+	if(x == y) {
+	cout <<" Is a palindrome" << endl;
+
+	}
+	else {
+	cout <<" Is not a palindrome" << endl;	
+	}
+}
+int main (){
+	palindrome();
+	return 0;
+}
